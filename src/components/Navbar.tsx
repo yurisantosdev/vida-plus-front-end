41b82b990c05f6d7c76eeb6abd94ebab4b61aa51
yurisantosdev <yuri.santos.cco@unochapeco.edu.br>
2025-08-01 11:14:44 -0300

    312

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,9 @@ import {
   Money,
   CarSimple,
   CalendarDots,
-  ListBullets
+  ListBullets,
+  User,
+  SignOut
 } from '@phosphor-icons/react'
 import React from 'react'
 import {
@@ -49,7 +51,7 @@ export default function Navbar() {
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            <DisclosureButton className="cursor-pointer group relative inline-flex items-center justify-center rounded-2xl p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset">
+            <DisclosureButton className="cursor-pointer group relative inline-flex items-center justify-center rounded-2xl p-2 text-gray-400 hover:bg-gray-900 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset">
               <span className="absolute -inset-0.5" />
               <List size={30} className="block size-6 group-data-open:hidden" />
               <X size={30} className="hidden size-6 group-data-open:block" />
@@ -73,8 +75,8 @@ export default function Navbar() {
                       aria-current={isActive ? 'page' : undefined}
                       className={classNames(
                         isActive
-                          ? 'bg-gray-700 text-white'
-                          : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          ? 'bg-gray-800 text-white'
+                          : 'text-gray-300 hover:bg-gray-900 hover:text-white',
                         'rounded-2xl px-3 py-2 text-sm font-medium cursor-pointer select-none'
                       )}>
                       <div className="flex justify-start items-center gap-3">
@@ -113,18 +115,24 @@ export default function Navbar() {
                 transition
                 className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-2xl bg-white py-1 shadow-lg ring-1 ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in">
                 <MenuItem>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden">
-                    Perfil
-                  </a>
+                  <div
+                    onClick={() => {
+                      alert('em desenvolvimento')
+                    }}
+                    className="cursor-pointer px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 rounded-2xl flex justify-start items-center gap-3">
+                    <User size={20} className="text-black" />
+                    <p>Perfil</p>
+                  </div>
                 </MenuItem>
                 <MenuItem>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden">
-                    Sair
-                  </a>
+                  <div
+                    onClick={() => {
+                      router.push('/')
+                    }}
+                    className="cursor-pointer px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 rounded-2xl flex justify-start items-center gap-2">
+                    <SignOut size={20} className="text-black" />
+                    <p>Sair</p>
+                  </div>
                 </MenuItem>
               </MenuItems>
             </Menu>
@@ -146,9 +154,9 @@ export default function Navbar() {
                 aria-current={isActive ? 'page' : undefined}
                 className={classNames(
                   isActive
-                    ? 'bg-gray-700 text-white'
-                    : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                  'block rounded-2xl px-3 py-2 text-base font-medium select-none'
+                    ? 'bg-gray-800 text-white'
+                    : 'text-gray-300 hover:bg-gray-900 hover:text-white',
+                  'block rounded-2xl px-3 py-2 text-base font-medium select-none cursor-pointer'
                 )}>
                 <div className="flex justify-start items-center gap-3">
                   {item.icon}
